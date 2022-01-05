7f8705e5a911f9413e28866aef69f2f22bb4da8e
wang314iu <wang314iu@gmail.com>
2022-01-05 03:22:04 -0500

    table

--- src/app/result-table/result-table.component.ts
+++ src/app/result-table/result-table.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { ReentrantProfile } from 'src/app/types/reentrantProfile';
 
 @Component({
   selector: 'app-result-table',
   templateUrl: './result-table.component.html',
   styleUrls: ['./result-table.component.scss']
 })
-export class ResultTableComponent implements OnInit {
+export class ResultTableComponent implements AfterViewInit {
+  @Input('data')
+  set data(val: ReentrantProfile[]) {
+    if (!!val && val !== []) {
+      this.dataSource.data = val;
+      console.log(val)
+    }
+  };
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  displayedColumns: string[] = ['docIdentifier', 'offenderName', 'pbppIdentifier', 'personalId', 'race', 'sex', 'sid', 'dob', 'icotsNo'];
+  dataSource = new MatTableDataSource<ReentrantProfile>([]);
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
-
 }
