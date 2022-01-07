7f4f0b160d788255729a7572f2837da7de769522
wang314iu <wang314iu@gmail.com>
2022-01-07 17:03:14 -0500

    save

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,6 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
+  personalInfo = [
+    'Address Line 1',
+    'Address Line 2',
+    'City',
+    'State',
+    'Zip + 4',
+    'Phone#',
+    'Cell#'
+  ];
+  employerInfo = [
+    "Address Line 1",
+    "Address Line 2",
+    " City:",
+    "State",
+    "Zip + 4",
+    " Employer",
+    "Employer Phone",
+  ];
+
+  supervision = [
+    "Grade of Supervision",
+    "PBPP Status Code",
+    "Status Code Date",
+    "Effective Delinquency Date",
+    "Last Contact Date",
+    "Last Contact Method",
+    "Last Drug Test",
+    "Acceptance Date",
+    "Test Type",
+    "Type of Case",
+    "PBPP Fees Due",
+    "Curfew",
+    "GPS",
+    "AOD Level",
+    "Sending State(s)",
+    "Receiving State"
+  ];
+
+
+  
+
+
+
 
   constructor() { }
 
